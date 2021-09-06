5aec6424f0807e37aa04c27228d604161fdda7f2
hectoralos <hectoralos@gmail.com>
2021-09-06 21:48:10 +0300

    correcció

--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -2,4 +2,4 @@ echo "==French->Catalan==========================";
 bash inconsistency.sh fra-cat ../../../apertium-fra/.deps/fra.dix > /tmp/fra-cat.testvoc; bash inconsistency-summary.sh /tmp/fra-cat.testvoc fra-cat 
 echo ""
 echo "==Catalan->French===========================";
-bash inconsistency.sh cat-fra ../../../apertium-cat/apertium-cat.cat.dix > /tmp/cat-fra.testvoc; bash inconsistency-summary.sh /tmp/cat-fra.testvoc cat-fra
+bash inconsistency.sh cat-fra ../../../apertium-cat/.deps/apertium-cat.cat.dix > /tmp/cat-fra.testvoc; bash inconsistency-summary.sh /tmp/cat-fra.testvoc cat-fra
